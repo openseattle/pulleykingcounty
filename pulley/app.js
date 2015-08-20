49203d99903dc5518d85e84273389a2b5c66fa74
Yonas Berhe <sanoy@Yonass-MacBook-Air.local>
2015-08-20 11:45:32 -0700

    map and nav

---
+++ pulley/app.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = 'pk.eyJ1Ijoic2Fub3kiLCJhIjoiNjgwMTM3YmE1YWIzZjJhMjMyZGE3Njk2MjdjZDA2MzAifQ.mIdbqT2ZUaFKyXqTfcdC1g';
+var map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([47.743589,-122.6440895], 10);
